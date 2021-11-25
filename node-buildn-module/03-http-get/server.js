e1a9ae46a83a37e1fa464d2a110eae4538c2ee9a
陈光游 <1070026284@qq.com>
2021-11-26 01:21:36 +0800

    日志打印使用

--- node-buildn-module/03-http-get/server.js
+++ node-buildn-module/03-http-get/server.js
@@ -1,9 +1,29 @@
 const http = require('http')
 const {resolve} = require("url");
+const queryString = require('querystring');
+const logsUtil = require('../utils/logs')
 
 const httpServe = http.createServer((res, req) => {
-    req.write('88888888888 99999999999999')
-    req.end()
+
+    let start = new Date()
+    let data = '';
+
+    res.on('data', (chunk) => {
+        data += chunk
+    })
+
+    res.on('end', () => {
+        // 设置请求头类型，text/html类型会解析html标签,application/json设置的返回类型为json集合，charset=utf-8设置编码格式
+        req.writeHead(200, {
+            'content-type': 'application/json;charset=utf-8'
+        })
+
+        debugger
+        logsUtil.logInfo(data)
+
+        req.write(JSON.stringify(queryString.parse(data)))
+        req.end()
+    })
 })
 
 httpServe.listen(8089, () => {
