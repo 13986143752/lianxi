e36ac86f05cc53cd1d6eb5b70f4bd2e076fcde0d
chenguangyou <cgy@null.com>
2021-11-30 12:00:20 +0800

    events常用api完善

--- node-buildn-module/09-events/app.js
+++ node-buildn-module/09-events/app.js
@@ -1,5 +1,6 @@
 // EventEmitter是一个类
 const EventEmitter = require('events')
+const {val} = require("cheerio/lib/api/attributes");
 
 // 创建子类并继承
 class myEventEmitter extends EventEmitter {}
@@ -10,6 +11,17 @@ const event = new myEventEmitter()
 event.on('play', (value) => {
     console.log(value)
 })
+
+// 仅触发一次
+event.once('play2', (val) => {
+    console.log(val)
+})
+
+// 关闭某一事件监听
+event.off('play', () => {})
+
+// 添加事件监听
+event.addListener('play3', () => {})
 
 // 触发事件
 event.emit('play', 'move')
